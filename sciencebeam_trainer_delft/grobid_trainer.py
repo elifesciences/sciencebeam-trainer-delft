47411b40af321a608512d72e33e04ae430d45b9f
Daniel Ecer <de-code@users.noreply.github.com>
2019-06-25 18:35:16 +0100

    more linting

--- sciencebeam_trainer_delft/grobid_trainer.py
+++ sciencebeam_trainer_delft/grobid_trainer.py
@@ -1,32 +1,34 @@
 # mostly copied from https://github.com/kermitt2/delft/blob/master/grobidTagger.py
-import json
 import logging
-import numpy as np
-from delft.utilities.Embeddings import Embeddings
-import delft.sequenceLabelling
-from delft.sequenceLabelling import Sequence
-from delft.utilities.Tokenizer import tokenizeAndFilter
-from sklearn.model_selection import train_test_split
-from delft.sequenceLabelling.reader import load_data_and_labels_crf_file
-from delft.sequenceLabelling.reader import load_data_and_labels_crf_string
-from delft.sequenceLabelling.reader import load_data_crf_string
-import keras.backend as K
 import argparse
 import time
 
-# from delft.utilities.cloud_support import patch_cloud_support
+from sklearn.model_selection import train_test_split
+import keras.backend as K
+
+from delft.sequenceLabelling import Sequence
+
+from delft.sequenceLabelling.reader import (
+    load_data_and_labels_crf_file
+)
 
 
-models = ['affiliation-address', 'citation', 'date', 'header', 'name-citation', 'name-header', 'software']
+MODELS = [
+    'affiliation-address', 'citation', 'date', 'header',
+    'name-citation', 'name-header', 'software'
+]
 
 
-# train a GROBID model with all available data 
+# train a GROBID model with all available data
 def train(
-        model, embeddings_name, architecture='BidLSTM_CRF', use_ELMo=False, input_path=None, output_path=None,
-        max_epoch=100, **kwargs): 
+        model, embeddings_name, architecture='BidLSTM_CRF', use_ELMo=False,
+        input_path=None, output_path=None,
+        max_epoch=100, **kwargs):
     print('Loading data...')
     if input_path is None:
-        x_all, y_all, _ = load_data_and_labels_crf_file('data/sequenceLabelling/grobid/'+model+'/'+model+'-060518.train')
+        x_all, y_all, _ = load_data_and_labels_crf_file(
+            'data/sequenceLabelling/grobid/' + model + '/' + model + '-060518.train'
+        )
     else:
         x_all, y_all, _ = load_data_and_labels_crf_file(input_path)
     x_train, x_valid, y_train, y_valid = train_test_split(x_all, y_all, test_size=0.1)
@@ -42,13 +44,15 @@ def train(
     if use_ELMo:
         model_name += '-with_ELMo'
 
-    model = Sequence(model_name, 
-                    max_epoch=max_epoch, 
-                    recurrent_dropout=0.50,
-                    embeddings_name=embeddings_name, 
-                    model_type=architecture,
-                    use_ELMo=use_ELMo,
-                    **kwargs)
+    model = Sequence(
+        model_name,
+        max_epoch=max_epoch,
+        recurrent_dropout=0.50,
+        embeddings_name=embeddings_name,
+        model_type=architecture,
+        use_ELMo=use_ELMo,
+        **kwargs
+    )
     # model.save = wrap_save(model.save)
 
     start_time = time.time()
@@ -57,19 +61,23 @@ def train(
     print("training runtime: %s seconds " % (runtime))
 
     # saving the model
-    if (output_path):
+    if output_path:
         print('saving model to:', output_path)
         model.save(output_path)
     else:
         model.save()
 
-# split data, train a GROBID model and evaluate it 
+
+# split data, train a GROBID model and evaluate it
 def train_eval(
-        model, embeddings_name, architecture='BidLSTM_CRF', use_ELMo=False, input_path=None, output_path=None,
+        model, embeddings_name, architecture='BidLSTM_CRF', use_ELMo=False,
+        input_path=None, output_path=None,
         fold_count=1, max_epoch=100, batch_size=20, **kwargs):
     print('Loading data...')
     if input_path is None:
-        x_all, y_all, _ = load_data_and_labels_crf_file('data/sequenceLabelling/grobid/'+model+'/'+model+'-060518.train')
+        x_all, y_all, _ = load_data_and_labels_crf_file(
+            'data/sequenceLabelling/grobid/' + model + '/' + model + '-060518.train'
+        )
     else:
         x_all, y_all, _ = load_data_and_labels_crf_file(input_path)
 
@@ -87,18 +95,20 @@ def train_eval(
 
     if use_ELMo:
         model_name += '-with_ELMo'
-        if model_name == 'software-with_ELMo' or model_name == 'grobid-software-with_ELMo':
+        if model_name in {'software-with_ELMo', 'grobid-software-with_ELMo'}:
             batch_size = 3
 
-    model = Sequence(model_name, 
-                    max_epoch=max_epoch, 
-                    recurrent_dropout=0.50,
-                    embeddings_name=embeddings_name, 
-                    model_type=architecture,
-                    use_ELMo=use_ELMo,
-                    batch_size=batch_size,
-                    fold_number=fold_count,
-                    **kwargs)
+    model = Sequence(
+        model_name,
+        max_epoch=max_epoch,
+        recurrent_dropout=0.50,
+        embeddings_name=embeddings_name,
+        model_type=architecture,
+        use_ELMo=use_ELMo,
+        batch_size=batch_size,
+        fold_number=fold_count,
+        **kwargs
+    )
 
     start_time = time.time()
 
@@ -115,17 +125,16 @@ def train_eval(
     model.eval(x_eval, y_eval)
 
     # saving the model
-    if (output_path):
+    if output_path:
         model.save(output_path)
     else:
         model.save()
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level='INFO')
-    # patch_cloud_support()
+def main():
     parser = argparse.ArgumentParser(
-        description = "Trainer for GROBID models")
+        description="Trainer for GROBID models"
+    )
 
     actions = ['train', 'tag', 'train_eval', 'eval']
     architectures = ['BidLSTM_CRF', 'BidLSTM_CNN', 'BidLSTM_CNN_CRF', 'BidGRU-CRF']
@@ -141,9 +150,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    model = args.model    
-    #if not model in models:
-    #    print('invalid model, should be one of', models)
+    model = args.model
+    if not model in MODELS:
+        print('invalid model, should be one of', MODELS)
 
     action = args.action
     if action not in actions:
@@ -157,26 +166,34 @@ if __name__ == "__main__":
     output = args.output
     input_path = args.input
 
-    # change bellow for the desired pre-trained word embeddings using their descriptions in the file 
+    # change bellow for the desired pre-trained word embeddings using their descriptions in the file
     # embedding-registry.json
-    # be sure to use here the same name as in the registry ('glove-840B', 'fasttext-crawl', 'word2vec'), 
+    # be sure to use here the same name as in the registry
+    # e.g. 'glove-840B', 'fasttext-crawl', 'word2vec',
     # and that the path in the registry to the embedding file is correct on your system
     embeddings_name = "glove-6B-50d"
-    kwargs = dict(batch_size=1, max_sequence_length=50, max_epoch=1)
+    default_args = dict(batch_size=1, max_sequence_length=50, max_epoch=1)
 
     if action == 'train':
         train(
-            model, embeddings_name, architecture=architecture, use_ELMo=use_ELMo, input_path=input_path, output_path=output,
-            **kwargs
+            model, embeddings_name, architecture=architecture, use_ELMo=use_ELMo,
+            input_path=input_path, output_path=output,
+            **default_args
         )
 
     if action == 'train_eval':
         if args.fold_count < 1:
             raise ValueError("fold-count should be equal or more than 1")
         train_eval(
-            model, embeddings_name, architecture=architecture, use_ELMo=use_ELMo, input_path=input_path, output_path=output, fold_count=args.fold_count,
-            **kwargs
+            model, embeddings_name, architecture=architecture, use_ELMo=use_ELMo,
+            input_path=input_path, output_path=output, fold_count=args.fold_count,
+            **default_args
         )
 
     # see https://github.com/tensorflow/tensorflow/issues/3388
     K.clear_session()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level='INFO')
+    main()

d8dd99003ee92ff5a834295177216fd8a0ae4e5f
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-02 22:17:44 +0100

    reuse TEST_DATA_PATH

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import os
 
 import pytest
 
+from .test_data import TEST_DATA_PATH
+
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_logging():
@@ -14,6 +16,6 @@ def setup_logging():
 @pytest.fixture(name='sample_train_file')
 def _sample_train_file():
     return os.path.join(
-        os.path.dirname(__file__),
-        'test_data/test-header.train'
+        TEST_DATA_PATH,
+        'test-header.train'
     )

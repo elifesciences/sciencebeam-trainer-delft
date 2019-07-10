b2e42f234d7e5b325e9c1dbdbb49a04d3187bcdb
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-10 21:15:18 +0100

    added optimizer type name

--- sciencebeam_trainer_delft/callbacks.py
+++ sciencebeam_trainer_delft/callbacks.py
@@ -97,13 +97,20 @@ class ModelWithMetadataCheckpoint(ModelSaverCallback):
         self.add_checkpoint_meta = add_checkpoint_meta
         super().__init__(save_fn=self._save_model, **kwargs)
 
-    def _save_model(self, epoch: int, logs: dict, **_):
-        meta = {
+    def _get_meta(self, logs: dict) -> dict:
+        optimizer = self.model.optimizer
+        optimizer_type = type(optimizer)
+        optimizer_fullname = '%s.%s' % (optimizer_type.__module__, optimizer_type.__name__)
+        return {
             **logs,
             'optimizer': {
-                'lr': float(K.get_value(self.model.optimizer.lr))
+                'type': optimizer_fullname,
+                'lr': float(K.get_value(optimizer.lr))
             }
         }
+
+    def _save_model(self, epoch: int, logs: dict, **_):
+        meta = self._get_meta(logs)
         LOGGER.info('meta: %s', meta)
         base_path = self.base_path.format(epoch=epoch + 1, **logs)
         self.model_saver.save_to(base_path, model=self.model, meta=meta)

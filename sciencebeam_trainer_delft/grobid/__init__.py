f58775b8f765a1c2c6ec10cab6bad9c12f34ef69
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-17 00:56:26 +0100

    disable warnings

---
+++ sciencebeam_trainer_delft/grobid/__init__.py
@@ -0,0 +1,1 @@
+import sciencebeam_trainer_delft.no_warn_if_disabled  # noqa, pylint: disable=unused-import
--- sciencebeam_trainer_delft/no_warn_if_disabled.py
+++ sciencebeam_trainer_delft/no_warn_if_disabled.py
@@ -1,9 +1,12 @@
+import os
 import sys
 import warnings
 
 
 def _hide_warnings_if_disabled():
-    if 'ignore' in sys.warnoptions:
+    # Note: usualy PYTHONWARNINGS is reflected in sys.warnoptions
+    #   but when run via JEP, sys.warnoptions appear to be empty
+    if 'ignore' in sys.warnoptions or os.getenv('PYTHONWARNINGS') == 'ignore':
         # respect no warning
         # see https://github.com/scikit-learn/scikit-learn/issues/2531
 

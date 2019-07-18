237df27af8b652bd86b223aa2f6439e6f65cfa72
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-18 12:50:52 +0100

    moved logging to utils

--- sciencebeam_trainer_delft/grobid/__init__.py
+++ sciencebeam_trainer_delft/grobid/__init__.py
@@ -1,5 +1,5 @@
 import sciencebeam_trainer_delft.no_warn_if_disabled  # noqa, pylint: disable=unused-import
-from sciencebeam_trainer_delft.logging import reset_logging
+from sciencebeam_trainer_delft.utils.logging import reset_logging
 
 
 reset_logging()

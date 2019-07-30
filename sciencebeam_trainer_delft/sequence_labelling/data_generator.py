e424258ed90c0c6f8dffeb72c70afef647c15d71
Daniel Ecer <de-code@users.noreply.github.com>
2019-07-30 10:23:45 +0100

    moved shuffle_arrays

--- sciencebeam_trainer_delft/sequence_labelling/data_generator.py
+++ sciencebeam_trainer_delft/sequence_labelling/data_generator.py
@@ -8,27 +8,11 @@ from delft.sequenceLabelling.preprocess import (
 )
 from delft.utilities.Tokenizer import tokenizeAndFilterSimple
 
+from sciencebeam_trainer_delft.utils.numpy import shuffle_arrays
 from sciencebeam_trainer_delft.sequence_labelling.preprocess import Preprocessor
 
 
 LOGGER = logging.getLogger(__name__)
-
-
-# https://stackoverflow.com/a/51526109/8676953
-def shuffle_arrays(arrays, set_seed=-1):
-    """Shuffles arrays in-place, in the same order, along axis=0
-
-    Parameters:
-    -----------
-    arrays : List of NumPy arrays.
-    set_seed : Seed value if int >= 0, else seed is random.
-    """
-    assert all(len(arr) == len(arrays[0]) for arr in arrays)
-    seed = np.random.randint(0, 2**(32 - 1) - 1) if set_seed < 0 else set_seed
-
-    for arr in arrays:
-        rstate = np.random.RandomState(seed)  # pylint: disable=no-member
-        rstate.shuffle(arr)
 
 
 def left_pad_batch_values(batch_values: np.array, max_sequence_length: int, dtype=None):
--- sciencebeam_trainer_delft/utils/numpy.py
+++ sciencebeam_trainer_delft/utils/numpy.py
@@ -7,3 +7,20 @@ def concatenate_or_none(arrays: List[np.array], **kwargs) -> np.array:
     if arrays[0] is None:
         return None
     return np.concatenate(arrays, **kwargs)
+
+
+# https://stackoverflow.com/a/51526109/8676953
+def shuffle_arrays(arrays, set_seed=-1):
+    """Shuffles arrays in-place, in the same order, along axis=0
+
+    Parameters:
+    -----------
+    arrays : List of NumPy arrays.
+    set_seed : Seed value if int >= 0, else seed is random.
+    """
+    assert all(len(arr) == len(arrays[0]) for arr in arrays)
+    seed = np.random.randint(0, 2**(32 - 1) - 1) if set_seed < 0 else set_seed
+
+    for arr in arrays:
+        rstate = np.random.RandomState(seed)  # pylint: disable=no-member
+        rstate.shuffle(arr)

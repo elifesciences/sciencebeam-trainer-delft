6c0a73e028bfaa38ee8fe44ca5a1012af02c0548
Daniel Ecer <de-code@users.noreply.github.com>
2019-08-08 15:20:16 +0100

    added lmdb argv workaround

--- sciencebeam_trainer_delft/grobid/__init__.py
+++ sciencebeam_trainer_delft/grobid/__init__.py
@@ -1,3 +1,4 @@
+import sciencebeam_trainer_delft.grobid.lmdb_argv_workaround  # noqa, pylint: disable=unused-import
 import sciencebeam_trainer_delft.utils.no_warn_if_disabled  # noqa, pylint: disable=unused-import
 from sciencebeam_trainer_delft.utils.logging import reset_logging
 
---
+++ sciencebeam_trainer_delft/grobid/lmdb_argv_workaround.py
@@ -0,0 +1,5 @@
+import sys
+
+# see https://github.com/jnwatson/py-lmdb/pull/207
+if not sys.argv:
+    sys.argv = ['']

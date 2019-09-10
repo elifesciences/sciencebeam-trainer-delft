61e95d00fedf895424d730ced65b2b7bebdce77e
Daniel Ecer <de-code@users.noreply.github.com>
2019-09-10 19:37:01 +0100

    added data file and flat text

--- sciencebeam_trainer_delft/sequence_labelling/debug.py
+++ sciencebeam_trainer_delft/sequence_labelling/debug.py
@@ -2,6 +2,8 @@ import os
 import json
 import logging
 import time
+from pathlib import Path
+from typing import List
 
 import numpy as np
 
@@ -12,9 +14,32 @@ LOGGER = logging.getLogger(__name__)
 SCIENCEBEAM_DELFT_TAGGING_DEBUG_OUT = "SCIENCEBEAM_DELFT_TAGGING_DEBUG_OUT"
 
 
+def to_data_lines(
+        features: np.array,
+        annotations) -> List[str]:
+    return [
+        ' '.join([token_annoation[0]] + list(token_features) + [token_annoation[1]])
+        for line_annotations, line_features in zip(annotations, features.tolist())
+        for token_annoation, token_features in zip(line_annotations, line_features)
+    ]
+
+
+def to_flat_text(texts: np.array) -> str:
+    return '\n'.join([
+        ' '.join(line_tokens)
+        for line_tokens in texts
+    ])
+
+
 class TagDebugReporter:
     def __init__(self, output_directory: str):
         self.output_directory = output_directory
+
+    def get_base_output_name(self, model_name: str) -> str:
+        return os.path.join(self.output_directory, 'sciencebeam-delft-%s-%s' % (
+            model_name,
+            round(time.time())
+        ))
 
     def report_tag_results(
             self,
@@ -22,21 +47,22 @@ class TagDebugReporter:
             features: np.array,
             annotations,
             model_name: str):
-        output_file = os.path.join(self.output_directory, 'sciencebeam-delft-%s-%s.data' % (
-            model_name,
-            round(time.time())
-        ))
+        output_file = self.get_base_output_name(model_name=model_name) + '.json'
         LOGGER.info('tagger, output_file: %s', output_file)
+        output_props = {
+            'texts': np.array(texts).tolist(),
+            'features': np.array(features).tolist() if features is not None else None,
+            'annotations': annotations,
+            'flat_text': to_flat_text(texts)
+        }
         with open(output_file, 'w') as fp:
-            json.dump(
-                {
-                    'texts': np.array(texts).tolist(),
-                    'features': np.array(features).tolist() if features is not None else None,
-                    'annotations': annotations
-                },
-                fp,
-                indent=4
-            )
+            json.dump(output_props, fp, indent=4)
+        if features is not None:
+            data_output_file = self.get_base_output_name(model_name=model_name) + '.data'
+            Path(data_output_file).write_text('\n'.join(to_data_lines(
+                features=features,
+                annotations=annotations
+            )))
 
 
 def get_tag_debug_reporter_if_enabled() -> TagDebugReporter:

f3ad9b31a6a9838b9a309051f494832664e79c7b
Daniel Ecer <de-code@users.noreply.github.com>
2020-02-17 16:26:05 +0100

    hide warnings by default (#146)

--- sciencebeam_trainer_delft/utils/no_warn_if_disabled.py
+++ sciencebeam_trainer_delft/utils/no_warn_if_disabled.py
@@ -4,10 +4,19 @@ import sys
 from sciencebeam_trainer_delft.utils.warnings import hide_warnings
 
 
-def _hide_warnings_if_disabled():
+def _is_warnings_if_disabled():
     # Note: usualy PYTHONWARNINGS is reflected in sys.warnoptions
     #   but when run via JEP, sys.warnoptions appear to be empty
     if 'ignore' in sys.warnoptions or os.getenv('PYTHONWARNINGS') == 'ignore':
+        return True
+    if not sys.warnoptions and not os.getenv('PYTHONWARNINGS'):
+        # ignore warnings by default
+        return True
+    return False
+
+
+def _hide_warnings_if_disabled():
+    if _is_warnings_if_disabled():
         # respect no warning
         # see https://github.com/scikit-learn/scikit-learn/issues/2531
         hide_warnings()

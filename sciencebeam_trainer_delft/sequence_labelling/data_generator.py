4054d7de6aaa1603f4893fa9adf86d5225c4aca9
Daniel Ecer <de-code@users.noreply.github.com>
2020-02-17 11:09:34 +0000

    apply max_sequence_length when tokenizing text

--- sciencebeam_trainer_delft/sequence_labelling/data_generator.py
+++ sciencebeam_trainer_delft/sequence_labelling/data_generator.py
@@ -100,10 +100,15 @@ class DataGenerator(keras.utils.Sequence):
             x_tokenized = []
             for i in range(0, max_iter):
                 tokens = tokenizeAndFilterSimple(sub_x[i])
-                if len(tokens) > max_length_x:
+                if len(tokens) > self.max_sequence_length:
+                    max_length_x = self.max_sequence_length
+                    # truncation of sequence at max_sequence_length
+                    tokens = tokens[:self.max_sequence_length]
+                elif len(tokens) > max_length_x:
                     max_length_x = len(tokens)
                 x_tokenized.append(tokens)
         else:
+            # for tokenized sequences, it is assumed that max_sequence_length is already applied
             for tokens in sub_x:
                 if len(tokens) > max_length_x:
                     max_length_x = len(tokens)

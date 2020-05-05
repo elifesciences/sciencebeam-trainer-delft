daaa4ada83e53782820313fb96c74791c4835aa6
Daniel Ecer <de-code@users.noreply.github.com>
2020-05-05 20:31:17 +0100

    allow separate OVERRIDE_MODEL env variables

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,17 +5,27 @@ set -e
 GROBID_MODELS_DIRECTORY=${GROBID_MODELS_DIRECTORY:-/opt/grobid/grobid-home/models}
 EMBEDDING_REGISTRY_PATH="${PROJECT_FOLDER}/embedding-registry.json"
 
-if [ ! -z "${OVERRIDE_MODELS}" ]; then
+# override models via OVERRIDE_MODELS or OVERRIDE_MODELS_*
+# the latter makes it easier to override multiple models as separate env variables
+env -0 | while IFS='=' read -r -d '' env_var_name env_var_value; do
+    if [[ -z "${env_var_value}" ]]; then
+        # skip empty values
+        continue
+    fi
+    if [[ "${env_var_name}" != "OVERRIDE_MODELS" ]] && [[ "${env_var_name}" != OVERRIDE_MODEL_* ]]; then
+        # skipping other env variable names
+        continue
+    fi
     if [ ! -d "${GROBID_MODELS_DIRECTORY}" ]; then
         echo "directory does not exist: ${GROBID_MODELS_DIRECTORY}"
         exit 1
     fi
-    echo "installing models: ${OVERRIDE_MODELS}"
+    echo "installing models: ${env_var_value} (${env_var_name})"
     python -m sciencebeam_trainer_delft.sequence_labelling.tools.install_models \
         --model-base-path=${GROBID_MODELS_DIRECTORY} \
-        --install "${OVERRIDE_MODELS}" \
+        --install "${env_var_value}" \
         --validate-pickles
-fi
+done
 
 if [ "${DISABLE_LMDB_CACHE}" == "1" ]; then
     echo "disabling lmdb cache: ${EMBEDDING_REGISTRY_PATH}"

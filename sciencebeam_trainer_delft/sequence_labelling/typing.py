9afe16ea5c303a7a34eb2f342b86b26a78baa6ed
Daniel Ecer <de-code@users.noreply.github.com>
2025-12-02 18:09:27 +0000

    declare T_Batch_Features as numpy array only (not list)

--- sciencebeam_trainer_delft/sequence_labelling/typing.py
+++ sciencebeam_trainer_delft/sequence_labelling/typing.py
@@ -1,8 +1,9 @@
 from typing import List, Union
 
 import numpy as np
+import numpy.typing as npt
 
 
 T_Batch_Tokens = List[List[str]]
-T_Batch_Features = Union[np.ndarray, List[List[List[str]]]]
+T_Batch_Features = npt.NDArray[np.float32]  # shape: (batch, seq_len, feat_dim)
 T_Batch_Labels = List[List[str]]
